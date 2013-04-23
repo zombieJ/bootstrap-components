0c85175381f6a12fef03ca0a5a76f1b50bf14376
Dead People <smith3@sina.com>
2013-04-23 23:57:40 +0800

    fit for IE9

--- src/common/bootstrap-common.js
+++ src/common/bootstrap-common.js
@@ -26,3 +26,13 @@ $._bc.get = function(options, key, defaultValue) {
 		return defaultValue;
 	}
 }
+
+// get the broswer version
+var userAgent = navigator.userAgent.toLowerCase();
+$._bc.browser = { 
+version: (userAgent.match( /.+(?:rv|it|ra|ie)[\/: ]([\d.]+)/ ) || [])[1], 
+safari: /webkit/.test( userAgent ), 
+opera: /opera/.test( userAgent ), 
+msie: /msie/.test( userAgent ) && !/opera/.test( userAgent ), 
+mozilla: /mozilla/.test( userAgent ) && !/(compatible|webkit)/.test( userAgent ) 
+};
--- src/fileuploader/bootstrap-fileuploader.js
+++ src/fileuploader/bootstrap-fileuploader.js
@@ -22,12 +22,17 @@ $.fn.extend({
 		var my = $(this);
 		my.hide();
 
-		var $label = $("<label class='fileuploader'>");
+		var _name = my.attr("name");
+
+		var $div = $("<div class='fileuploader'>");
+		var $label = $("<label class='fileuploader-region'>");
 		var $button = $("<button class='btn file' type='button'>File</button>");
 		var $tips = $("<span class='tips'>Select File</span>");
 		var $region = $("<div>");
 		var $files_region = $("<div class='fileuploader-items'>");
 
+		$div.insertAfter(my);
+		$div.append(my);
 		$label.insertAfter(my);
 		$button.appendTo($label);
 		$tips.appendTo($label);
@@ -49,8 +54,10 @@ $.fn.extend({
 				var _fileNum = $files_region.find(".fileuploader-item input[type='file']").length;
 				if(_max != -1 && _fileNum >= _max) {
 					$button.attr("disabled", "disabled");
+					if($._bc.browser.msie) my.attr("disabled", "disabled");
 				} else {
 					$button.removeAttr("disabled");
+					if($._bc.browser.msie) my.removeAttr("disabled");
 				}
 				if(_fileNum == 0)
 					$tips.html("Select File");
@@ -59,11 +66,34 @@ $.fn.extend({
 			}
 		}
 
+		function ieSuck() {
+			if($._bc.browser.msie) {
+				my.show();
+				$label.prepend(my);
+				$div.addClass("ie");
+				my.css("width", $button.css("width"));
+				my.css("height", $button.css("height"));
+				my.addClass("fileuploader-ori");
+				$button.insertBefore($label);
+				var $tmp_lbl = $("<label>");
+				$tmp_lbl.insertAfter($button);
+				$tmp_lbl.append($button);
+
+				return true;
+			}
+			return false;
+		}
+
 		if(!_muti_upload) {		// if enable upload one file
 			markUploader(my, function(){
 				var filename = getFilename($(this).val());
 				$tips.html(filename);
+				if(filename == "") $tips.html("Select File");
 			});
+
+			ieSuck();	// IE SUCKS
+
+			// init tips if page loaded has already has selected one
 			if(my.val() != "") {
 				var filename = getFilename(my.val());
 				$tips.html(filename);
@@ -78,38 +108,59 @@ $.fn.extend({
 			my.removeAttr("multiple");
 
 			refreshEnable();
+
+			if(ieSuck()) {						// IE SUCKS
+				my.removeAttr("name");
+
+				markUploader(my, function(){
+					if(addFileMark($(this))) {
+						my = $(this).clone(true);
+						$(this).hide();
+						$(this).attr("name", _name);
+						$label.prepend(my);
+
+						refreshEnable();
+					}
+				});
+			}
+		}
+
+		function addFileMark($input) {
+			var filename = getFilename($input.val());
+			var $div_file = $("<div class='fileuploader-item'>");
+			var $rm = $("<button class='close' type='button'>&times;</button>");
+			var $filename = $("<span class='tips'>");
+
+			var has = false;
+			$(".fileuploader-item input[type='file']").each(function(){
+				var _filename = getFilename($(this).val());
+				if(filename == _filename) has = true;
+			});
+			if(has) {
+				alert("Can't upload same file.");
+				return false;
+			}
+
+			$filename.html(filename);
+			$input.attr("multiple", "multiple");
+			$rm.click(function(){
+				$div_file.remove();
+				refreshEnable();
+			});
+
+			$div_file.appendTo($files_region);
+			$input.appendTo($div_file);
+			$rm.appendTo($div_file);
+			$filename.appendTo($div_file);
+
+			return true;
 		}
 
 		$button.click(function(){
 			if(_muti_upload) {
 				var $file = my.clone();
 				markUploader($file, function(){
-					var filename = getFilename($(this).val());
-					var $div_file = $("<div class='fileuploader-item'>");
-					var $rm = $("<button class='close' type='button'>&times;</button>");
-					var $filename = $("<span class='tips'>");
-
-					var has = false;
-					$(".fileuploader-item input[type='file']").each(function(){
-						var _filename = getFilename($(this).val());
-						if(filename == _filename) has = true;
-					});
-					if(has) {
-						alert("Can't upload same file.");
-						return;
-					}
-
-					$filename.html(filename);
-					$(this).attr("multiple", "multiple");
-					$rm.click(function(){
-						$div_file.remove();
-						refreshEnable();
-					});
-
-					$div_file.appendTo($files_region);
-					$(this).appendTo($div_file);
-					$rm.appendTo($div_file);
-					$filename.appendTo($div_file);
+					addFileMark($(this));
 
 					refreshEnable();
 				});
