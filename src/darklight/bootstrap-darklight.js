72ea4a1b0164d2b69aa2ddfd04c85606ab3a5374
Dead People <smith3@sina.com>
2013-04-29 00:21:54 +0800

    enable muti dark

--- src/darklight/bootstrap-darklight.js
+++ src/darklight/bootstrap-darklight.js
@@ -5,6 +5,8 @@ options:
 					true				enable close the modal on click backdrop.
 	event:			[event]				String: when element call the target function, dark background will hide.
 										default is "click" event, it will auto close.
+	start:			boolean				default is null, false to prevent show right now.
+										to show it call $(tgt).showDarklight();
 callback:			[function]			it will trigger event your set in option-event, return true to trigger close.
 										defalut is always return true.
 */
@@ -25,32 +27,63 @@ $.fn.extend({
 
 		var _backdrop = $._bc.get(_options, "backdrop", "static");
 		var _event = $._bc.get(_options, "event", "click");
+		var _start = _options.start;
+
 		if(_callback == null) _callback = function() {return true;}
 
 		var my = $(this);
+		var $next = null;
 		var $dark = $._bc.vals.darklight.darkdrop;
 
-		// first open to show animation
-		if(!$._bc.vals.darklight.opened) {
-			$("body").append($dark);
-			setTimeout(function(){
-				$dark.addClass("in");
-			},1);
+		if(_start == null) {
+			// first open to show animation
+			if(!$._bc.vals.darklight.opened) {
+				$("body").append($dark);
+				setTimeout(function(){
+					$dark.addClass("in");
+				},1);
 
-			$._bc.vals.darklight.opened = true;
+				$._bc.vals.darklight.opened = true;
+			}
+
+			my.addClass("darklight-top");
+
+			// call to check close the darklight
+			my.bind(_event + EVENT_ATTACH, function(){
+				var _ret = _callback.call($(this));
+				if(_ret) {console.log($next);
+					$(this).unbind(_event + EVENT_ATTACH);
+					my.removeClass("darklight-top");
+
+					if($next == null) {
+						$dark.removeClass("in");
+						setTimeout(function(){
+							$dark.hide();
+						},300);
+					} else {
+						$next.showDarklight();
+					}
+				}
+			});
 		}
 
-		my.addClass("darklight-top");
+		my.nextDarklight = function(obj, options, callback){
+			var $_next = $(obj);
+			if(options == null) options = new Object();
+			options.start = false;
+			$next = $_next.darklight(options, callback);
 
-		my.bind(_event + EVENT_ATTACH, function(){
-			var _ret = _callback.call($(this));
-			if(_ret) {
-				$(this).unbind(_event + EVENT_ATTACH);
-				$dark.removeClass("in");
-				setTimeout(function(){
-					$dark.hide();
-				},300);
-			}
-		});
+			return $next;
+		}
+
+		my.showDarklight = function() {
+			if(options == null) options = new Object();
+			options.start = null;
+			my.darklight(options, callback);
+		}
+
+		return my;
 	}
 });
+
+$.fn.extend({});
