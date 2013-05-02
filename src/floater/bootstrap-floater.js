8d329415ce8a39e6ffe678daf5b818560e405f7c
U-DeadPeople-PC\DeadPeople <DeadPeople@DeadPeople-PC.(none)>
2013-05-02 23:12:04 +0800

    add collepse for floater & enhance doc folder

--- src/floater/bootstrap-floater.js
+++ src/floater/bootstrap-floater.js
@@ -1,6 +1,7 @@
 /* options:
 	after:		[element]			float after target element, default null
 	margin:		[number]			margin of target elment, default is 0
+	collapse:	[boolean]			create a span to fill the ori element position, default is false
 */
 
 // init function
@@ -14,14 +15,15 @@ $.fn.extend({
 		var my = $(this);
 		var $after = $._bc.get(_options, "after", null);
 		var _margin = $._bc.get(_options, "margin", 0);
+		var _collapse = $._bc.get(_options, "collapse", false);
 		var $tgt = $(document);
 
 		if(my.length == 0) return;
 
 		if($after != null) $after = $($after);
 
-		var my_fake = $("<span>");
-		my_fake.css("display", my.css("display"));my_fake.css("zoom", "1");
+		var my_fake = $("<span class='floater-collapse'>");
+		my_fake.css("display", my.css("display"));
 		my_fake.css("margin", my.css("margin"));
 		my_fake.outerHeight(my.outerHeight());
 		my_fake.outerWidth(my.outerWidth());
@@ -39,21 +41,21 @@ $.fn.extend({
 
 			if($after == null) {
 				if($(this).scrollTop() + _margin > pos.top) {
-					my_fake.show();
+					if(!_collapse) my_fake.show();
 					my.addClass("floater");
 					my.css("top", _margin);
 				} else {
-					my_fake.hide();
+					if(!_collapse) my_fake.hide();
 					my.removeClass("floater");
 				}
 			} else {
 				var _top = $after.offset().top + $after.outerHeight();
 				if(_top + _margin > pos.top) {
-					my_fake.show();
+					if(!_collapse) my_fake.show();
 					my.addClass("floater");
 					my.css("top", (_top - $(this).scrollTop() + _margin) + "px");
 				} else {
-					my_fake.hide();
+					if(!_collapse) my_fake.hide();
 					my.removeClass("floater");
 				}
 			}
