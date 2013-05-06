20cf629105ab66d2b117e7f311369b87219cd28f
Dead People <smith3@sina.com>
2013-05-06 23:43:09 +0800

    fill notify component

--- src/notify/bootstrap-notify.js
+++ src/notify/bootstrap-notify.js
@@ -2,6 +2,9 @@
 options:
 	title:			string				specify title of dialog.
 	content:		element				specify content of dialog.
+	position:		string				"left", "right", "top", "bottom". You can mix then up as "left,top" (it's same as "top,left")
+	type:			string				"normal", "warning", "info", "error", "success"
+	timeout:		number				default: 5000, hiden speed.
 
 callback:			[function]			it will trigger event when user close this dialog by click the return button.
 										return boolean of confirm, and false of alert and close button.
@@ -15,9 +18,62 @@ $.extend({
 		var _options = vars.options;
 		var _callback = vars.callback;
 
-		var _title = $._bc.get(_options, "title", "");
+		var _title = $._bc.get(_options, "title", "Notification");
 		var _content = $._bc.get(_options, "content", "");
+		var _position = $._bc.get(_options, "position", "right,top");
+		var _type = $._bc.get(_options, "type", "normal");
+		var _timeout = $._bc.get(_options, "timeout", 5000);
 
-		
+		var $notification = $("<div class='alert notification-body alert-block'>");
+		var $btn = $("<button type='button' class='close'>¡Á</button>");
+
+		// alert position
+		if(_position.indexOf("left") != -1) $notification.addClass("left");
+		if(_position.indexOf("right") != -1) $notification.addClass("right");
+		if(_position.indexOf("top") != -1) $notification.addClass("top");
+		if(_position.indexOf("bottom") != -1) $notification.addClass("bottom");
+
+		// alert color
+		if(_type == "normal") $notification.addClass("alert-normal");
+		if(_type == "error") $notification.addClass("alert-error");
+		if(_type == "success") $notification.addClass("alert-success");
+		if(_type == "info") $notification.addClass("alert-info");
+
+		$notification.append($btn);
+
+		if(_title == "") _title = "&nbsp;";
+		if(typeof(_title) == 'string') _title = "<h5>" + _title + "</h5>";
+		$notification.append(_title);
+		if(_content != "") $notification.append(_content);
+
+		// fade in
+		$("body").append($notification);
+		$notification.hide();
+		$notification.fadeIn();
+
+		if(_timeout > 0) {
+			var _tt;
+			function setAutoFadeOut() {
+				_tt = window.setTimeout(function(){
+					$btn.click();
+				}, _timeout);
+			}
+
+			setAutoFadeOut();
+			$notification.mouseenter(function(){
+				window.clearTimeout(_tt);
+			});
+			$notification.mouseleave(function(){
+				setAutoFadeOut();
+			});
+		}
+
+		// fade out
+		$btn.click(function(){
+			$notification.fadeOut();
+			window.setTimeout(function(){
+				$notification.remove();
+			}, 1000);
+		});
 	}
 });
