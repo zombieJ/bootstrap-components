9482a0fdc4191eae52dc2afc51398aa609e85702
Dead People <smith3@sina.com>
2013-05-11 14:34:47 +0800

    fin swith doc

--- src/switch/bootstrap-switch.js
+++ src/switch/bootstrap-switch.js
@@ -5,53 +5,56 @@ $.fn.extend({
 		var vars = $._bc.vars(options);
 		var _options = vars.options;
 
-		var my = $(this);
-		my.hide();
+		// for each checkbox
+		$(this).each(function(){
+			var my = $(this);
+			my.hide();
 
-		var _readonly = my.prop('readonly');
-		var _disabled = my.prop('disabled');
+			var _readonly = my.prop('readonly');
+			var _disabled = my.prop('disabled');
 
-		var $bac = $("<div class='progress switch'>");
-		var $lbl = $("<button class='label'></button>");
-		var $btn = $("<button type='button' class='btn spiner'>");
+			var $bac = $("<div class='progress switch'>");
+			var $lbl = $("<button class='label'></button>");
+			var $btn = $("<button type='button' class='btn spiner'>");
 
-		$bac.insertAfter(my);
-		$lbl.appendTo($bac);
-		$btn.appendTo($bac);
+			$bac.insertAfter(my);
+			$lbl.appendTo($bac);
+			$btn.appendTo($bac);
 
-		function refreshStatus() {
-			var _on = my.prop("checked");
-			if(_on) {
-				$bac.removeClass("off");
-				$bac.addClass("on");
-				$lbl.html("ON");
+			function refreshStatus() {
+				var _on = my.prop("checked");
+				if(_on) {
+					$bac.removeClass("off");
+					$bac.addClass("on");
+					$lbl.html("ON");
 
-				var _left = $bac.width() - $btn.outerWidth();
-				$btn.css("left", _left + "px");
+					var _left = $bac.width() - $btn.outerWidth();
+					$btn.css("left", _left + "px");
+				} else {
+					$bac.removeClass("on");
+					$bac.addClass("off");
+					$lbl.html("OFF");
+
+					$btn.css("left", 0);
+				}
+			}
+			refreshStatus();
+
+			if(_readonly || _disabled) {
+				$bac.addClass("readonly");
+				$btn.addClass("disabled");
+
+				if(_disabled) {
+					$bac.addClass("disabled");
+				}
 			} else {
-				$bac.removeClass("on");
-				$bac.addClass("off");
-				$lbl.html("OFF");
-
-				$btn.css("left", 0);
+				$bac.click(function(){
+					var _on = !my.prop("checked");
+					my.prop("checked", _on);
+					refreshStatus();
+					my.change();
+				});
 			}
-		}
-		refreshStatus();
-
-		if(_readonly || _disabled) {
-			$bac.addClass("readonly");
-			$btn.addClass("disabled");
-
-			if(_disabled) {
-				$bac.addClass("disabled");
-			}
-		} else {
-			$bac.click(function(){
-				var _on = !my.prop("checked");
-				my.prop("checked", _on);
-				refreshStatus();
-				my.change();
-			});
-		}
+		});
 	}
 });
