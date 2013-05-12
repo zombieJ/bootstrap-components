8c5a8f8ef9e926b65ca8a2405221ff052562b009
Dead People <smith3@sina.com>
2013-05-12 16:27:31 +0800

    checkbox

---
+++ src/checkbox/bootstrap-checkbox.js
@@ -0,0 +1,14 @@
+// init function
+!function ($) {
+	$(document).on("change.bootstrapcomponent.checkbox", "input[type='checkbox']", function(event){
+		var _value = $(this).prop('checked');
+
+		var $parent = $(this).parent();
+		var $checkbox = $parent.find(".checkbox-mark");
+		if(_value) {
+			$checkbox.addClass("checked");
+		} else {
+			$checkbox.removeClass("checked");
+		}
+	});
+}(window.jQuery);
